49d71e2b55cd16f4d0eed04294a69ba37ce731b1
CyrilD <doyencyril@gmail.com>
2012-10-22 14:10:22 +0200

    creation de villeController

---
+++ inssetAirline/application/controllers/villeController.php
@@ -0,0 +1,18 @@
+<?php
+
+class VilleController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function listeAction()
+    {
+        // action body
+    }
+
+
+}
+
