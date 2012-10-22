d7013e7429d1fca2da4ea03bee522df06fb30f67
Pampril <doyencyril@gmail.com>
2012-10-22 14:46:02 +0300

    Update inssetAirline/application/Bootstrap.php
    ajout des fonctions initSession et initConfig

--- inssetAirline/application/Bootstrap.php
+++ inssetAirline/application/Bootstrap.php
@@ -3,5 +3,21 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+  public function run()
+	{
+		parent::run();
+	}
+	
+	protected function _initConfig()
+	{
+		Zend_Registry::set('configs', new Zend_Config($this->getOptions()));
+	}
+	
+	protected function _initSession()
+	{
+		$session = new Zend_Session_Namespace('inssetAirline', true);
+		return $session;
+	}
 }
+
 
