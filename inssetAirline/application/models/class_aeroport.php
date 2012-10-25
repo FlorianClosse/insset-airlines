cd6272e31aff22b60ccb87717d2eb5ccaf21260a
CyrilD <doyencyril@gmail.com>
2012-10-25 18:06:29 +0200

    ajout des dependances de classes

--- inssetAirline/application/models/class_aeroport.php
+++ inssetAirline/application/models/class_aeroport.php
@@ -3,5 +3,10 @@ class Aeroport extends Zend_Db_Table_Abstract
 {
 	protected $_name='aeroport';
 	protected $_primary=array('idAeroport');
+	
+	protected $_referenceMap = array(
+			'Ville' => array(
+					'columns' => 'idVille',
+					'refTableClass' =>'ville'));
 }
 ?>
--- inssetAirline/application/models/class_user.php
+++ inssetAirline/application/models/class_user.php
@@ -3,5 +3,10 @@ class User extends Zend_Db_Table_Abstract
 {
 	protected $_name='user';
 	protected $_primary=array('idUser');
+	
+	protected $_referenceMap = array(
+			'Aeroport' => array(
+					'columns' => 'idAeroport',
+					'refTableClass' =>'aeroport'));
 }
 ?>
