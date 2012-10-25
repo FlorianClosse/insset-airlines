27814e1d45c375ee01ddf905bf54f37975f168d0
CyrilD <doyencyril@gmail.com>
2012-10-25 17:50:53 +0200

    ajout de la relation avec la table pays

--- inssetAirline/application/models/class_ville.php
+++ inssetAirline/application/models/class_ville.php
@@ -3,5 +3,10 @@ class Ville extends Zend_Db_Table_Abstract
 {
 	protected $_name='ville';
 	protected $_primary=array('idVille');
+	
+	protected $_referenceMap = array(
+			'Pays' => array(
+					'columns' => 'idPays',
+					'refTableClass' =>'pays'));
 }
 ?>
