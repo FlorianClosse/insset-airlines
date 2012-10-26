4bf0b334234dd8532fe2dd820d531fd35bfaa494
FlorianClosse <florian.closse@yahoo.fr>
2012-10-26 16:38:38 +0200

    maj classe vol

--- inssetAirline/application/models/class_vol.php
+++ inssetAirline/application/models/class_vol.php
@@ -3,5 +3,24 @@ class Vol extends Zend_Db_Table_Abstract
 {
 	protected $_name='vol';
 	protected $_primary=array('idVol');
+
+
+	protected $_referenceMap = array(
+			'aeroportDepart'=>array(
+					'columns'=>'idAeroport',
+					'refTableClass'=>'aeroport'),
+			'aeroportArrivee'=>array(
+					'columns'=>'idAeroport',
+					'refTableClass'=>'aeroport'),
+			'idAvion'=>array(
+					'columns'=>'idAvion',
+					'refTableClass'=>'Avion'),
+			'idDate'=>array(
+					'columns'=>'idDate',
+					'refTableClass'=>'dateVolAlaCarte'),
+			'id_copilote'=>array(
+					'columns'=>'id_pilote',
+					'refTableClass'=>'Pilote')
+	);
 }
 ?>
