a83fca5f02643920c138ee280025b0cce572d337
FlorianClosse <florian.closse@yahoo.fr>
2012-10-26 16:49:38 +0200

    classe revision

---
+++ inssetAirline/application/models/class_revision.php
@@ -0,0 +1,13 @@
+<?php
+class Revision extends Zend_Db_Table_Abstract
+{
+	protected $_name='revision';
+	protected $_primary=array('idRevision');
+	
+	protected $_referenceMap = array(
+			'idJournal'=>array(
+					'columns'=>'idJournal',
+					'refTableClass'=>'journalDeBord')
+	);
+}
+?>
