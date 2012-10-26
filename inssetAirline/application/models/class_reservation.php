87f3904758844aa2cfe8d127fea7a436ad695210
FlorianClosse <florian.closse@yahoo.fr>
2012-10-26 16:48:07 +0200

    classe réservation

--- inssetAirline/application/models/class_reservation.php
+++ inssetAirline/application/models/class_reservation.php
@@ -3,5 +3,11 @@ class Reservation extends Zend_Db_Table_Abstract
 {
 	protected $_name='reservation';
 	protected $_primary=array('idReservation');
+	
+	protected $_referenceMap = array(
+			'idJournal'=>array(
+					'columns'=>'idJournal',
+					'refTableClass'=>'journalDeBord')
+	);
 }
 ?>
