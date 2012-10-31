8536094df0e27274f9f4f7a8a4879bfc14533d11
matth02100 <matthieu.derache@gmail.com>
2012-10-31 15:37:29 +0100

    classe dateVolAlaCarte

---
+++ inssetAirline/application/models/class_dateVolALaCarte.php
@@ -0,0 +1,10 @@
+<?php
+/*
+*	classe realisee par matthieu
+*/
+	class dateVolAlaCarte extends Zend_Db_Table_Abstract
+	{
+		protected $_name='datevolalacarte';
+		protected $_primary=array('idDate');
+	}
+?>
