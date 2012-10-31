be3a014e55ee11a82346145f92027dc40e0d0aa7
matth02100 <matthieu.derache@gmail.com>
2012-10-31 15:32:05 +0100

    classe commentaire vol

---
+++ inssetAirline/application/models/class_commentaireVol.php
@@ -0,0 +1,15 @@
+<?php
+/*
+*	classe realisee par matthieu
+*/
+	class CommentaireVol extends Zend_Db_Table_Abstract
+	{
+		protected $_name='commentaireVol';
+		protected $_primary=array('idCommentaireVol');
+		
+		protected $_referenceMap = array(
+				'idJournalDeBord' => array(
+						'columns' => 'idJournalDeBord',
+						'refTableClass' =>'journaldebord'));
+	}
+?>
