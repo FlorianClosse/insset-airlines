213fa4a07c8067768265fe234090e902e11a51be
nicolasBeze <nicolas.beze@laposte.net>
2012-12-12 11:42:13 +0100

    Création du menu principal

--- inssetAirline/application/controllers/IndexController.php
+++ inssetAirline/application/controllers/IndexController.php
@@ -11,6 +11,41 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         $this->_helper->actionStack('login', 'index', 'default', array());
+        
+        $menu = $this->_getParam('menu');
+        if(isset($menu))
+        {
+        	switch ($menu)
+        	{
+        		case "index":
+        			$this->_helper->actionStack('index', 'index', 'default', array());
+        			break;
+        			
+        		case "commercial":
+        			$this->_helper->actionStack('index', 'commercial', 'default', array());
+        			break;
+        			 
+        		case "strategique":
+        			$this->_helper->actionStack('index', 'strategique', 'default', array());
+        			break;
+        		
+        		case "technique":
+        			$this->_helper->actionStack('index', 'technique', 'default', array());
+        			break;
+        		
+        		case "logistique":
+        			$this->_helper->actionStack('index', 'logistique', 'default', array());
+        			break;
+        		
+        		case "drh":
+        			$this->_helper->actionStack('index', 'drh', 'default', array());
+        			break;
+        		
+        		case "maintenance":
+        			$this->_helper->actionStack('fermerligne', 'maintenance', 'default', array());
+        			break;
+        	}
+        }
     }
     public function loginAction(){
     	$this->_helper->viewRenderer->setResponseSegment('login');
