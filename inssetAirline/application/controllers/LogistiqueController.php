6fab98c89da4efdb5200e7cda87b267bcc2a1d1a
CyrilD <doyencyril@gmail.com>
2012-12-13 17:04:02 +0100

    ajout de la liste deroulante pour les aeroport

--- inssetAirline/application/controllers/LogistiqueController.php
+++ inssetAirline/application/controllers/LogistiqueController.php
@@ -21,7 +21,7 @@ class LogistiqueController extends Zend_Controller_Action
 		
 		//******** affichage de commentaire (bouton valider) ********
 		$compteur = 0;		
-		$commentaire = new CommentaireVol();
+		$commentaire = new Commentairevol;
 		$lesCommentaires = $commentaire->fetchAll();
 		
 		$vol = new Vol;
@@ -64,7 +64,7 @@ class LogistiqueController extends Zend_Controller_Action
 		}
 		if(isset($_POST['EnvoyerCommentaire']))
 		{
-							$commentaire = new CommentaireVol();
+							$commentaire = new Commentairevol;
 								
 							$comvol = $commentaire->createRow();
 							$comvol->idCommentaireVol = '';
@@ -179,7 +179,7 @@ class LogistiqueController extends Zend_Controller_Action
 			}		
  			$vol->setMultiOptions($tableauVols);
 			
- 			$journalDeBord = new JournalDeBord();
+ 			$journalDeBord = new Journaldebord;
  			$lesjournaldebord = $journalDeBord->fetchAll();
 			$journalDeBord = new Zend_Form_Element_Select('journaldebord');
 			$journalDeBord ->setLabel('Choisir un id journal de bord');
