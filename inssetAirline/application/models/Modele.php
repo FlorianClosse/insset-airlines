60a49b31ee275f196b374ad2835c48db020a5e82
matthieu <matthieu@matthieu>
2013-01-24 11:57:42 +0100

    ajout méthode selectOne

--- inssetAirline/application/models/Modele.php
+++ inssetAirline/application/models/Modele.php
@@ -6,5 +6,12 @@ class Modele extends Zend_Db_Table_Abstract
 {
 	protected $_name = 'modele';
 	protected $_primary = 'idModele';
+	
+	public function selectOne($idModele)
+	{
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$requete = 'SELECT * FROM modele WHERE idModele='.$idModele;
+		return $db->query($requete)->fetchAll();
+	}
 }
 ?>
