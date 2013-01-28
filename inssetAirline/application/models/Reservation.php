6a9e2eee2049d8c02967bf3ab46aa38ffe9e46f2
CyrilD <doyencyril@gmail.com>
2013-01-28 16:01:35 +0100

    Ajout des filtres au controller commercial

--- inssetAirline/application/models/Reservation.php
+++ inssetAirline/application/models/Reservation.php
@@ -9,5 +9,56 @@ class Reservation extends Zend_Db_Table_Abstract
 					'columns'=>'idJournal',
 					'refTableClass'=>'journalDeBord')
 	);
+	
+	public function getAeroportDepart($depart)
+	{
+		$requete = 	 'select *
+					 from journalDeBord J, vol V, reservation R
+					 where R.idJournal = J.idJournalDeBord
+					 and J.idVol = V.idVol
+					 and V.aeroportDepart =\''.$depart.'\';';
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$datas = $db->query($requete)->fetchAll();
+		//Zend_Debug::dump($datas) ;
+		return $datas;
+	}
+	
+	public function getAeroportArrivee($arrive)
+	{
+		$requete = 	 'select *
+					 from journalDeBord J, vol V, reservation R
+					 where R.idJournal = J.idJournalDeBord
+					 and J.idVol = V.idVol
+					 and V.aeroportArrivee =\''.$arrive.'\';';
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$datas = $db->query($requete)->fetchAll();		
+		return $datas;
+	}
+	
+	public function getAeroportDepartArrivee($depart,$arrive)
+	{
+		$requete = 	 'select *
+					 from journalDeBord J, vol V, reservation R
+					 where R.idJournal = J.idJournalDeBord
+					 and J.idVol = V.idVol
+					 and V.aeroportDepart =\''.$depart.'\'
+					 and V.aeroportArrivee =\''.$arrive.'\'
+					 and statutReservation = "en attente";';
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$datas = $db->query($requete)->fetchAll();				
+		return $datas;
+	}
+	
+	public function getLesAeroports()
+	{
+		$requete = 	 'select *
+					 from journalDeBord J, vol V, reservation R
+					 where R.idJournal = J.idJournalDeBord
+					 and J.idVol = V.idVol;';
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$datas = $db->query($requete)->fetchAll();
+		//Zend_Debug::dump($datas) ;
+		return $datas;
+	}
 }
 ?>
