6c28951df167dc1261537cc3c1fd69a1df2cc88a
matthieu <matthieu@matthieu>
2013-01-29 18:36:46 +0100

    ajout de méthode dans la classe user

--- inssetAirline/application/models/User.php
+++ inssetAirline/application/models/User.php
@@ -8,5 +8,24 @@ class User extends Zend_Db_Table_Abstract
 			'idAeroport' => array(
 					'columns' => 'idAeroport',
 					'refTableClass' =>'aeroport'));
+	
+	/*protected $_referenceMap = array(
+			'service' => array(
+					'columns' => 'idService',
+					'refTableClass' =>'service'));*/
+	
+	public function selectOne($idUser)
+	{
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$requete = 'SELECT * FROM user WHERE idUser='.$idUser;
+		return $db->query($requete)->fetchAll();
+	}
+	
+	public function selectAll()
+	{
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$requete = 'SELECT * FROM user ';
+		return $db->query($requete)->fetchAll();
+	}
 }
 ?>
