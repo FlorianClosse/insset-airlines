19e6f7d2b6cfed9774a6259a2de970aa80970d35
matthieu <matthieu@matthieu>
2013-01-29 18:38:13 +0100

    creation controller admin et de ses vues

---
+++ inssetAirline/application/controllers/AdminController.php
@@ -0,0 +1,63 @@
+<?php
+	class AdminController extends Zend_Controller_Action
+	{	
+		public function indexAction()
+		{
+			
+		}
+		
+		public function aeroportAction()
+		{
+			$aeroport = new Aeroport();
+			$pagination = Zend_Paginator::factory($aeroport->selectAll());
+			$pagination->setCurrentPageNumber($this->_getParam('page'));
+			$pagination->setItemCountPerPage(15);
+			$this->view->lesaeroports = $pagination;
+		}
+		
+		public function userAction()
+		{
+			$user = new User();
+			$pagination = Zend_Paginator::factory($user->selectAll());
+			$pagination->setCurrentPageNumber($this->_getParam('page'));
+			$pagination->setItemCountPerPage(15);
+			$this->view->lesusers = $pagination;
+		}
+		
+		public function villeAction()
+		{
+			$ville = new Ville();
+			$pagination = Zend_Paginator::factory($ville->selectAll());
+			$pagination->setCurrentPageNumber($this->_getParam('page'));
+			$pagination->setItemCountPerPage(15);
+			$this->view->lesvilles =$pagination;
+		}
+		
+		public function paysAction()
+		{
+			$pays = new Pays();
+			$pagination = Zend_Paginator::factory($pays->selectAll());
+			$pagination->setCurrentPageNumber($this->_getParam('page'));
+			$pagination->setItemCountPerPage(15);
+			$this->view->lespays = $pagination;
+		}
+		
+		public function modeleAction()
+		{
+			$modele = new Modele();
+			$pagination = Zend_Paginator::factory($modele->selectAll());
+			$pagination->setCurrentPageNumber($this->_getParam('page'));
+			$pagination->setItemCountPerPage(15);
+			$this->view->lesmodeles = $pagination;
+		}
+		
+		public function brevetAction()
+		{
+			$brevet = new Brevet();
+			$pagination = Zend_Paginator::factory($brevet->selectAll());
+			$pagination->setCurrentPageNumber($this->_getParam('page'));
+			$pagination->setItemCountPerPage(15);
+			$this->view->lesbrevets = $pagination;
+		}
+	}
+?>
