32d5ac9e99876de327072f065446717614edaecb
FlorianClosse <florian.closse@yahoo.fr>
2013-01-30 19:57:14 +0100

    modif form deco

--- inssetAirline/application/forms/Fdeco.php
+++ inssetAirline/application/forms/Fdeco.php
@@ -1,7 +1,6 @@
 <?php
 class Fdeco extends Zend_Form
 {
-	
 	public function init(){
 		/* Parametrer le formulaire */
     	$this->setMethod('post')->setAction('/index/deco');
@@ -12,7 +11,8 @@ class Fdeco extends Zend_Form
 		$submit = new Zend_Form_Element_Submit('submit');
 		$submit->setDecorators($decoratorElem2);
 		$submit->setAttrib('id', 'submitbutton')
-		->setLabel('Deconnexion');
+		->setAttrib('class','submitbuttonedeco')
+		->setLabel('');
 	
 		$elements = array($submit);
 		$this->addElements($elements);
